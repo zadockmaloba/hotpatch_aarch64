65b1696bdd487d0a4407134da2cd904960339587
Zadock Maloba <malobazadock@gmail.com>
2023-09-04 17:03:19 +0300

    Add new submodules

--- main.cpp
+++ main.cpp
@@ -9,7 +9,52 @@
 #include <cstring>
 #include <sys/mman.h>
 
-void hotpatch(void *target, void *replacement) {
+#include "plthook/plthook.h"
+
+//__attribute__ ((aligned(8)))
+__attribute__ ((noinline))
+
+size_t plt_hotpatch(void *original_function, void *new_function) {
+    plthook_t *plthook;
+    
+    if (plthook_open_by_address(&plthook, reinterpret_cast<void*>(original_function)) != 0) {
+        printf("plthook_open error: %s\n", plthook_error());
+        return -1;
+    }
+    if (plthook_replace(plthook, "recv", (void*)new_function, (void**)&original_function) != 0) {
+        printf("plthook_replace error: %s\n", plthook_error());
+        plthook_close(plthook);
+        return -1;
+    }
+#ifndef WIN32
+    // The address passed to the fourth argument of plthook_replace() is
+    // available on Windows. But not on Unixes. Get the real address by dlsym().
+    //recv_func = (ssize_t (*)(int, void *, size_t, int))dlsym(RTLD_DEFAULT, "recv");
+#endif
+    plthook_close(plthook);
+}
+
+size_t plt_hotpatch_2(void *original_function, void *new_function) {
+    plthook_t *plthook;
+    
+    /*if (plthook_open_by_address(&plthook, reinterpret_cast<void*>(original_function)) != 0) {
+        printf("plthook_open error: %s\n", plthook_error());
+        return -1;
+    }*/
+    if (plthook_replace_builtin(plthook, (void*)new_function, (void**)&original_function) != 0) {
+        printf("plthook_replace error: %s\n", plthook_error());
+        plthook_close(plthook);
+        return -1;
+    }
+#ifndef WIN32
+    // The address passed to the fourth argument of plthook_replace() is
+    // available on Windows. But not on Unixes. Get the real address by dlsym().
+    //recv_func = (ssize_t (*)(int, void *, size_t, int))dlsym(RTLD_DEFAULT, "recv");
+#endif
+    plthook_close(plthook);
+}
+
+void aarch64_hotpatch(void *target, void *replacement) {
 
     auto page_size = getpagesize();
 
@@ -18,10 +63,10 @@ void hotpatch(void *target, void *replacement) {
     // Align the target address to an 8-byte boundary
     // uintptr_t aligned_target = ((uintptr_t)target + 7) & ~7;
 
-    // Calculate the page address containing the target
-    void *page = reinterpret_cast<void*>(aligned_target);
-
     assert(((uintptr_t)aligned_target & 0x07) == 0); // Ensure 8-byte alignment
+
+    // Calculate the page address containing the target
+    void *page = reinterpret_cast<void*>(target);
 
     // Make the page writable and executable
     if(mprotect(page, page_size, PROT_WRITE | PROT_EXEC) != 0) {
@@ -30,7 +75,7 @@ void hotpatch(void *target, void *replacement) {
     std::cout << "Memory protected \n";
 
     // Calculate the relative offset for the branch instruction
-    uintptr_t rel = (uintptr_t)replacement - aligned_target;
+    uintptr_t rel = (uintptr_t)replacement - (uintptr_t)target;
 
     std::cout << "Relative distance: " << rel << "\n";
 
@@ -39,14 +84,32 @@ void hotpatch(void *target, void *replacement) {
     uint32_t instr = 0xE0000000; // B opcode
     instr |= ((rel >> 2) & 0x03FFFFFF); // Offset in words, not bytes
 
-    std::cout << "Attempting to patch target: " << aligned_target << "\n";
+    std::cout << "Attempting to patch target: " << target << "\n";
     // Write the instruction to the target address
     // *(uint32_t *)aligned_target = instr;
-    memcpy(target, replacement, rel);
+    //memcpy(target, replacement + 4, rel);
+    union {
+        uint8_t bytes[8];
+        uint64_t value;
+    } instruction = {{0xe9, rel >> 0, rel >> 8, rel >> 16, rel >> 24}};
+    *(uint64_t *)target = instruction.value;
     std::cout << "Patched target \n";
 
     // Restore the page protection to executable only
     mprotect(page, page_size, PROT_EXEC);
+}
+
+void x86_hotpatch(void *target, void *replacement) {
+    assert(((uintptr_t)target & 0x07) == 0);
+    void *page = (void *)((uintptr_t)target & ~0xfff);
+    mprotect(page, 4096, PROT_WRITE | PROT_EXEC);
+    uint32_t rel = (char *)replacement - (char *)target - 5;
+    union {
+        uint8_t bytes[8];
+        uint64_t value;
+    } instruction = {{0xe9, 0}};
+    *(uint64_t *)target = instruction.value;
+    mprotect(page, 4096, PROT_EXEC);
 }
 
 int original_function() {
@@ -59,11 +122,11 @@ int new_function() {
 
 int main() {
     // Example usage:
-    void* f1 = reinterpret_cast<void*>((std::size_t&)original_function);
-    void* f2 = reinterpret_cast<void*>((std::size_t&)new_function);
 
-    hotpatch(f1, f2);
+    plt_hotpatch((void*)&original_function, (void*)new_function);
 
+    x86_hotpatch((void*)original_function, (void*)new_function);
+    //aarch64_hotpatch((void*)&original_function, (void*)new_function);
     // Test the patched function
     printf("Original function: %d\n", original_function());
 
