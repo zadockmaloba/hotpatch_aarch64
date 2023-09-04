27a40a09e9bddeda7292132e87d343a42ad03afc
zadockmaloba <malobazadock@gmail.com>
2023-09-04 05:28:53 -0400

    Align the address.

--- main.cpp
+++ main.cpp
@@ -2,46 +2,67 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <vector>
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
 #include <sys/mman.h>
 
 void hotpatch(void *target, void *replacement) {
+
+    auto page_size = getpagesize();
+
+    auto aligned_target = reinterpret_cast<std::size_t>(target) & (~(page_size - 1));
+
     // Align the target address to an 8-byte boundary
-    uintptr_t aligned_target = ((uintptr_t)target + 7) & ~7;
+    // uintptr_t aligned_target = ((uintptr_t)target + 7) & ~7;
 
     // Calculate the page address containing the target
-    void *page = (void *)(aligned_target & ~0xFFF);
+    void *page = reinterpret_cast<void*>(aligned_target);
 
     assert(((uintptr_t)aligned_target & 0x07) == 0); // Ensure 8-byte alignment
 
     // Make the page writable and executable
-    mprotect(page, 4096, PROT_WRITE | PROT_EXEC);
+    if(mprotect(page, page_size, PROT_WRITE | PROT_EXEC) != 0) {
+	    std::abort;
+    }
+    std::cout << "Memory protected \n";
 
     // Calculate the relative offset for the branch instruction
     uintptr_t rel = (uintptr_t)replacement - aligned_target;
+
+    std::cout << "Relative distance: " << rel << "\n";
 
     // Generate the AARCH64 branch instruction
     // AARCH64 uses the B instruction
     uint32_t instr = 0xE0000000; // B opcode
     instr |= ((rel >> 2) & 0x03FFFFFF); // Offset in words, not bytes
 
+    std::cout << "Attempting to patch target: " << aligned_target << "\n";
     // Write the instruction to the target address
-    *(uint32_t *)aligned_target = instr;
+    // *(uint32_t *)aligned_target = instr;
+    memcpy(target, replacement, rel);
+    std::cout << "Patched target \n";
 
     // Restore the page protection to executable only
-    mprotect(page, 4096, PROT_EXEC);
+    mprotect(page, page_size, PROT_EXEC);
+}
+
+int original_function() {
+	return 42;
+}
+
+int new_function() {
+	return 84;
 }
 
 int main() {
     // Example usage:
-    int original_function() {
-        return 42;
-    }
+    void* f1 = reinterpret_cast<void*>((std::size_t&)original_function);
+    void* f2 = reinterpret_cast<void*>((std::size_t&)new_function);
 
-    int new_function() {
-        return 84;
-    }
-
-    hotpatch(original_function, new_function);
+    hotpatch(f1, f2);
 
     // Test the patched function
     printf("Original function: %d\n", original_function());
